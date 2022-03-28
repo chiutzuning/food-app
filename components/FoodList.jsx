a357a7e079d08b7deaa9b914ccc6ca8081dfe869
Tzuning Chiu <chiutzuning@gmail.com>
2022-03-28 21:17:30 +0200

    solved bug

--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -7,7 +7,7 @@ const FoodList = ({ foodList }) => {
     <div className={styles.container}>
     <h1 className={styles.title}>THE BEST JAPANESE RESTAURANT IN TOWN</h1>
       <p className={styles.desc}>
-      Sushi makes a great choice for an office lunch, as you're bound to find something
+      Sushi makes a great choice for an office lunch, as you are bound to find something
       that suits different members of the team. Vegetarian sushi is common, and so are vegan
       options, which are often made with flavour-packed vegetables like avocados and mushrooms.
       </p>
@@ -16,8 +16,8 @@ const FoodList = ({ foodList }) => {
               <FoodCard key={sushi._id} sushi={sushi} />
             ))}
       </div>
-    </div>
-    
+gi    </div>
+
   );
 };
 
